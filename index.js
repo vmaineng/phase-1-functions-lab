10dc45a4d1db3cf5325a087c693ffb1786bf1b8f
Mai Vang <vmaineng@gmail.com>
2022-04-26 09:47:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+let blocks;
+let feet;
+
+function distanceFromHqInBlocks(distance){
+    if (distance > 42) {
+        let blocks = distance % 42
+        return blocks
+    } else {
+        let blocks = 42 - distance
+    return blocks
+    }
+};
+
+function distanceFromHqInFeet(distance) {
+    if (distance > 42) {
+        let feet = ((distance - 42) * 264)
+        return feet
+    } else {
+        let feet = ((42 - distance) * 264)
+        return feet
+    }
+};
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264
+    }
+};
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <=2000) {
+        return (distance - 400) * .02
+    } else if (distance > 2000 && distance < 2500){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+};
